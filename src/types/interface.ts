88d9111dc6950ae887ccf5f62920ed8414cd4bc5
ThiagoMarzari <thiagomarzari1@gmail.com>
2025-06-22 22:19:25 -0300

    feat: add ItemProps and LogProps interfaces for item and log management

---
+++ src/types/interface.ts
@@ -0,0 +1,16 @@
+export interface ItemProps {
+  nome: string;
+  codigo: string;
+  usuarioAtual: string;
+  codigoUsuario: string;
+}
+
+export interface LogProps {
+  id: number
+  tipo: string
+  itemNome: string
+  itemCodigo: string
+  usuarioNome: string
+  usuarioCodigo: string
+  dataEmprestimo: string
+}
